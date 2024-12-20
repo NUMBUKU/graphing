bddf06c9ae9bf4efc75f9b9ecb6ad7f5b133134c
NUMBUKU <112706502+NUMBUKU@users.noreply.github.com>
2024-12-20 15:12:20 +0100

    added graph lib files

---
+++ graph.c
@@ -0,0 +1,13 @@
+# include "graph.h"
+
+int create_graph (Vector * x_array, Vector * y_array){
+
+
+    return 0;
+}
+
+int show (){
+
+
+    return 0;
+}
---
+++ graph.h
@@ -0,0 +1,19 @@
+# ifndef GRAPH_H
+# define GRAPH_H
+
+# include <raylib.h>
+# include <stdio.h>
+
+# include "arr.h"
+
+/// @brief Graphs the the two given arrays against each other.
+/// @param x_array The array of data to be plotted on the horizontal axis.
+/// @param y_array The array of data to be plotted on the vertical axis.
+/// @return 1 if there was an error, 0 otherwise.
+int create_graph (Vector * x_array, Vector * y_array);
+
+/// @brief Shows the plot
+/// @return void
+void show ();
+
+# endif // GRAPH_H
